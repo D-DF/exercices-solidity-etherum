7d43282d64b0045f05ce27fa69d4f44f7aee5710
D-DF <daviddefreitas0192@gmail.com>
2021-05-11 16:51:16 +0200

    ok

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -5,12 +5,12 @@ pragma solidity ^0.8.0;
 
 contract SimpleStorage {
     uint256 private _storedData;
-    
-    function storedData() public {
-        return _storedData;
+
+    function setStoredData(uint256 value) public {
+       return value = _storedData;
     }
     
-    function setStoredData(uint256 value) public returns(uint256){
-       return value = _storedData;
+    function storedData() public returns(uint256) {
+        return _storedData;
     }
 }
